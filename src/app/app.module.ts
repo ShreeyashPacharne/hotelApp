6ce6c0de08ed26784a0b906f7ba8486d80ad6290
Shreeyash <shreepac99@gmail.com>
2023-06-09 23:54:54 +0530

    created the components for the modules present on the nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { BranchesComponent } from './branches/branches.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactusComponent,
+    BranchesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
