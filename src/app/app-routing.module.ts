6a6776d10a505a15f0b8a6c9528292b8feb00210
Shreeyash <shreepac99@gmail.com>
2023-06-10 12:00:56 +0530

    Add the routing to the pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BranchesComponent } from './branches/branches.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'branches', component: BranchesComponent},
+  {path: 'contact', component: ContactusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
